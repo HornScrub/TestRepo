5f86a9a643433a70e3fe400c2a25a7f2c21637d2
ben <benjaminstephan1997@gmail.com>
2024-05-07 18:45:55 -0500

    New little testy code

--- bash_extra3.sh
+++ bash_extra3.sh
@@ -3,10 +3,14 @@
 echo -n "What's up? Enter a number: "
 
 read number
+number2
 
 if [ $((number % 2)) -eq 0 ] 
 then
-    echo "Even"
+    number2 -eq [ number / 2 ]
+    echo "$number times 4 equals $number2"
 else
-    echo "Odd"
+    number2 -eq [ number / 2 ]
+    echo "$number divided by 2 equals $number2"
 fi
+
